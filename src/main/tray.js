546dc117a764cf4f024b5a1016600052e744b8f6
flbraun <mail@chiron-online.de>
2023-10-29 19:33:01 +0100

    Persist user settings (in tray icon) to disk
    Persist user settings (in tray icon) to disk using electron-store.
    
    By default the settings file will be located at `%APPDATA%\poe-palette\config.json`.
    
    Resolves #5

--- src/main/tray.js
+++ src/main/tray.js
@@ -1,5 +1,6 @@
 const { Tray, Menu, app } = require('electron')
 const path = require('node:path')
+const { userSettings } = require('./storage')
 
 exports.createTray = (leftClickCallback) => {
     const tray = new Tray(path.join(__dirname, '..', '..', 'assets', 'mirror.png'))
@@ -8,16 +9,61 @@ exports.createTray = (leftClickCallback) => {
     const contextMenu = Menu.buildFromTemplate([
         { label: `PoE Palette v${app.getVersion()}`, enabled: false },
         { type: 'separator' },
-        { type: 'checkbox', label: 'Wiki', checked: true },
-        { type: 'checkbox', label: 'PoEDB', checked: true },
-        { type: 'checkbox', label: 'PoENinja', checked: true },
-        { type: 'checkbox', label: 'TFT', checked: true },
-        { type: 'checkbox', label: 'Tools', checked: true },
+        {
+            type: 'checkbox',
+            label: 'Wiki',
+            checked: userSettings.get('wikiEnabled'),
+            click: (menuItem) => userSettings.set('wikiEnabled', menuItem.checked),
+        },
+        {
+            type: 'checkbox',
+            label: 'PoEDB',
+            checked: userSettings.get('poedbEnabled'),
+            click: (menuItem) => userSettings.set('poedbEnabled', menuItem.checked),
+        },
+        {
+            type: 'checkbox',
+            label: 'PoENinja',
+            checked: userSettings.get('ninjaEnabled'),
+            click: (menuItem) => userSettings.set('ninjaEnabled', menuItem.checked),
+        },
+        {
+            type: 'checkbox',
+            label: 'TFT',
+            checked: userSettings.get('tftEnabled'),
+            click: (menuItem) => userSettings.set('tftEnabled', menuItem.checked),
+        },
+        {
+            type: 'checkbox',
+            label: 'Tools',
+            checked: userSettings.get('toolsEnabled'),
+            click: (menuItem) => userSettings.set('toolsEnabled', menuItem.checked),
+        },
         { type: 'separator' },
-        { type: 'radio', label:'Challenge', checked: true },
-        { type: 'radio', label:'Challenge Hardcore' },
-        { type: 'radio', label:'Standard' },
-        { type: 'radio', label:'Hardcore' },
+        {
+            type: 'radio',
+            label: 'Challenge',
+            checked: userSettings.get('league') === 'challenge',
+            click: () => userSettings.set('league', 'challenge'),
+        },
+        {
+            type: 'radio',
+            label: 'Challenge Hardcore',
+            checked: userSettings.get('league') === 'challengehc',
+            click: () => userSettings.set('league', 'challengehc'),
+        },
+        {
+            type: 'radio',
+            label: 'Standard',
+            checked: userSettings.get('league') === 'standard',
+            click: () => userSettings.set('league', 'standard'),
+        },
+        {
+            type: 'radio',
+            label: 'Hardcore',
+            checked: userSettings.get('league') === 'hardcore',
+            click: () => userSettings.set('league', 'hardcore'),
+        },
         { type: 'separator' },
         { label: 'Toggle visibility', click: leftClickCallback },
         { type: 'separator' },
