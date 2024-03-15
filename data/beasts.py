52b13f3f547dc8fa1f4cdd52a420ddceb4c9f3b0
flbraun <mail@chiron-online.de>
2024-03-15 17:32:56 +0100

    fix generation of beasts (caused by changes in 31fc7f3)

--- data/beasts.py
+++ data/beasts.py
@@ -4,11 +4,12 @@ from .leagues import League
 from .ninja import get_ninja_index, make_ninja_url
 from .trade import make_trade_url
 from .types import NinjaCategory
-from .utils import Entry, make_wiki_url
+from .utils import DefaultHTTPSession, Entry, make_wiki_url
 
 
 def get_beasts(league: League) -> Generator[Entry, None, None]:
-    index = get_ninja_index(league)
+    with DefaultHTTPSession() as ninja_session:
+        index = get_ninja_index(ninja_session, league)
 
     for beast in index.raw[NinjaCategory.BEASTS]:
         yield Entry(
